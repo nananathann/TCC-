a67a37e43b96d50cc859cbddf865cf2075a20ff9
nathan <nathandiasbr17@gmail.com>
2023-08-11 06:31:44 -0300

    pagina de inicio com css quase pronto

---
+++ src/home.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './css/home.css'
+import Barra from './images/Barramenu.png'
+import robokids from './images/robokids.png'
+import robo from './images/robo-12.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <header>
+
+  <img src={Barra} alt="barra menu" />
+
+<div className="logo">
+  <img src={robokids} alt="pequeno robokids" />
+  </div>
+    </header>
+
+    
+
+    <main>
+    <div className="barra1"></div>
+    
+    <div className="LogoInicial">
+      <img src={robokids} alt="robokids" />
+    </div>
+
+<div className="Robo">
+      <img src={robo} alt="robo da robokids" />
+    </div>
+
+    <section>
+    <p>Site voltado à apresentação do projeto</p>
+    </section>
+
+    <div className="linkinho">
+      <a href="">Objetivos</a>
+      </div>
+
+    <h1> Público <br/>Alvo</h1>
+    
+    <div className="publico">
+      <p>Já existem carrinhos, com sensor de proximidade, e segue linha, porém nenhum deles foram criados com o mesmo intuito do nosso, os objetivos de cada um é diferente um do outro</p>
+      </div>
+  
+      
+
+      </main>
+      <div className="logoFinal">
+        <img src={robokids} alt="robokids" />
+      </div>
+      
+      <div className="barra2"></div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/sobre.md
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './css/sobre.css'
+
+function Sobre() {
+  return (
+    <>
+    <h1>nana testando</h1>
+    </>
+  )
+}
