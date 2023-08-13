73daeb80ba69228a97b3957f4f2c32ff239e3bcf
nathan <nathandiasbr17@gmail.com>
2023-08-12 23:01:48 -0300

    html do sobre pronto

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './home.jsx'
+import Sobre from './sobre.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/sobre.jsx
+++ src/sobre.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import './css/sobre.css'
 import Barra from './images/Barramenu.png'
 import robokids from './images/robokids.png'
+import carrinho from './images/carrinhofeito.png'
 
-function sobre() {
+
+function Sobre() {
   const [count, setCount] = useState(0)
   return (
     <>
@@ -16,10 +18,24 @@ function sobre() {
   </div>
 
     </header>
-
+    
     <main>
+
       <h1>Sobre</h1>
+      
+      <img src={carrinho} alt="exemplo de carrinho que ja foi feito" />
+
+      <div className="p1">
+        <p>Já existem carrinhos, com sensor de proximidade, e segue linha, porém nenhum deles foram criados com o mesmo intuito do nosso, os objetivos de cada um é diferente um do outro</p>
+        </div>
+
+        <div className="p2">
+          <p>esse carrinho é um exemplo de carrinhos que tem a base muito parecida com o carrinho que iremos apresentar, tanto que sera usado como base para o nosso projeto</p>
+        </div>
     </main>
     </>
   )
 }
+
+
+export default Sobre
