6628f33bda852a34407c3712995a717af3d4d50d
nathan <nathandiasbr17@gmail.com>
2023-09-05 00:22:39 -0300

    objetivos

--- src/objetivos.jsx
+++ src/objetivos.jsx
@@ -62,18 +62,19 @@ function Objetivos() {
     </div>
     
   <main>
-    <div className="barradeitada"></div>
+    <div className="deitada"></div>
+
+    <section className="pa1">
+        <p>O nosso objetivo com este projeto é apresentar nossa ideia robótica, usando um pequeno carrinho e algumas pecas de Arduíno, nos produzimos um carrinho, que ira estimular o neném a andar e/ou engatinhar, e também o fara se divertir brincando.</p>
+    </section>
 
     <img src={bonecos} alt="" className="bonequinhos" />
 
-    <section className="p1">
-        <p>O nosso objetivo com este projeto é apresentar nossa ideia robótica, usando um pequeno carrinho e algumas pecas de Arduíno, nos produzimos um carrinho, que ira estimular o neném a andar e/ou engatinhar, e também o fara se divertir brincando.</p>
-    </section>
     <h1>Objetivos</h1>
 
-    <div className="barradeitada2"></div>
+    <div className="deitada2"></div>
     
-    <section className="p2">
+    <section className="pa2">
         <p>o carrinho terá um sensor de proximidade onde o fará se afastar assim que o neném se aproximar dele, fazendo assim ele ficar seguindo ele, como uma brincadeira de pega-pega. Como item de segurança o carrinho ele será também um segue linha, para ele não sair da área delimitada com tapetes de borracha, para assim evitar acidentes com o neném</p>
     </section>
 
